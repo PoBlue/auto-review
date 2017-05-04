bdbcc5ebec914f3d8a11f7190f1005103cb7d9f5
blues <blues@blues.com>
2017-05-04 18:40:18 +0800

    完成 ui 构建

--- regex_review/web_handler.py
+++ regex_review/web_handler.py
@@ -2,21 +2,23 @@
 """
 hadler for web page
 """
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, make_response
 from main_center import Parser
 from base_func import get_filepaths, get_immediate_subdirectories
 app = Flask(__name__)
 
 
 @app.route('/review', methods=['GET', 'POST'])
-def request_handle():
+def review():
     """
     handle request from web
     """
     if request.method == 'POST':
-        return handle_post(request.form)
+        selected_path = request.cookies.get('selected-path')
+        return handle_post(request.form, selected_path)
     else:
-        return handle_get()
+        selected_path = request.args.get('selected_project')
+        return handle_get(selected_path)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -24,36 +26,52 @@ def setting_handle():
     """
     set project dir path
     """
-    project_dir = request.args.get('project-path')
-    if project_dir != "" and project_dir is not None:
-        all_project_paths = get_immediate_subdirectories(project_dir + "/")
-        return render_template('select.html', project_paths=all_project_paths)
+    if request.method == 'POST':
+        selected_project_path = request.form["select-project-path"]
+        project_dir = request.cookies.get('project-path')
+        selected_path = project_dir + '/' + selected_project_path + '/'
+        return redirect(url_for('review', selected_project=selected_path))
     else:
-        return render_template('setdir.html')
+        project_dir = request.args.get('project-path')
+        if not has_value(project_dir):
+            project_dir = request.cookies.get('project-path')
+        if has_value(project_dir):
+            all_project_paths = get_immediate_subdirectories(project_dir + "/")
+            resp = make_response(render_template('select.html',
+                                                 project_paths=all_project_paths))
+            resp.set_cookie('project-path', project_dir)
+            return resp
+        else:
+            return render_template('setdir.html')
 
 
-def handle_post(form):
+def has_value(value):
+    return value != "" and value is not None
+
+
+def handle_post(form, selected_path):
     """
     handle post method
     """
-    test_dir_path = "/Users/blues/Desktop/ArcadeGame-4/"
     parser = Parser()
     parser.set_review_args(form.getlist("file-paths"),
-                           test_dir_path,
+                           selected_path,
                            form["data-path"])
     parser.creat_chorme_excfile()
     return "sucessful"
 
 
-def handle_get():
+def handle_get(selected_path):
     """
     handle get method
     """
-    test_dir_path = "/Users/blues/Desktop/ArcadeGame-4/"
-    all_file_paths = get_filepaths(test_dir_path)
+    all_file_paths = get_filepaths(selected_path)
     all_data_paths = get_filepaths("data/")
-    return render_template("index.html", file_paths=all_file_paths,
-                           data_paths=all_data_paths)
+    resp = make_response(render_template("index.html",
+                                         file_paths=all_file_paths,
+                                         data_paths=all_data_paths))
+    resp.set_cookie('selected-path', selected_path)
+    return resp
 
 if __name__ == '__main__':
     app.debug = True
